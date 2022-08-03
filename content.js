88be61d9627caaa60f6b8d7e3751115a1940f6a8
aymyo <ayman.yousfan01@estudiant.upf.edu>
2022-08-03 20:53:36 +0200

    updated selectors

--- content.js
+++ content.js
@@ -1,120 +1,148 @@
-let debug = false;
+let DEBUG_ON = false;
 
-//An array that will store the option values: 
+//An array that will store the option values:
 //[explore-toggle,suggestions-toggle,stories,color-toggle,hexadecimal color code]
 let option_values;
 
-//DOM elements
-let NavLinks;
-let Suggestions;
-let Stories;
-let Main;
-let Nav;
-let Feed;
-let FeedLoader;
+// DOM elements that vary between pages
+let mainSectionElement;
+let navbarElement;
+let feedBackgroundElement;
+
+const selectNavLinkDirect = () => document.querySelector('a[href*="/direct/inbox/"]');
+const selectNavLinkActivity = () => document.querySelector('a[href*="/accounts/activity"]');
+const selectNavLinkExplore = () => document.querySelector('section > nav a[href*="/explore/"]');
+
+const selectSuggestionsBox = () =>
+  document.querySelector('a[href*="/explore/people/"]').closest("div").nextSibling;
+const selectSuggestionsLink = () =>
+  document.querySelector('a[href*="/explore/people/"]').closest("div");
+
+const selectStoriesBox = () => document.querySelectorAll('div[role="presentation"]')[0];
+const selectStoriesArrow = () =>
+  document.querySelectorAll('div[role="presentation"]')[0].nextSibling;
+
+const selectFeedBox = () => document.querySelector("article").closest("div");
+const selectFeedLoader = () => document.querySelector('[data-visualcompletion="loading-state"]');
+
+//IDEA: bloquejar crides a la API
+//IDEA: injectar nou header
+//IDEA: get info from github to communicate with users
+//IDEA: use testing playground
+//IDEA: get the selectors from a file calling an API, so we can update them quickly (might break chromes policy)
 
 //This function gets the most recent option values
-function UpdateOptions(){
-    chrome.storage.sync.get(['options'], function(result) {
-        option_values = result.options;
-        if(debug){console.log(option_values);}
-        //Default initial values
-        if(!option_values){
-            let option_values = [true,true, false, false, "#fafafa", false];   
-            chrome.storage.sync.set({options: option_values}, function() {
-            console.log("Antigram's default values set to:" + option_values);
-            });
-        }
-    });}
+const updateOptions = () => {
+  chrome.storage.sync.get(["options"], function (result) {
+    option_values = result.options;
+    if (DEBUG_ON) {
+      console.log(option_values);
+    }
+    //Default initial values
+    if (!option_values) {
+      let option_values = [true, true, false, false, "#fafafa", false];
+      chrome.storage.sync.set({ options: option_values }, function () {
+        console.log("Antigram's default values set to:" + option_values);
+      });
+    }
+  });
+};
 
 //This function blocks or shows the selected element depending on the option_value
-function BlockElement(element, value) {
-    if(value && element != null){
-        element.style.display= "none";
-    } else if(element != null){
-        element.style.display= "block";
-    }
-}
+const blockElement = (element, value) => {
+  if (value && element != null) {
+    element.style.display = "none";
+  } else if (element != null) {
+    element.style.display = "block";
+  }
+};
 
 //Changes Background Color if the setting is activated / restores the original if not
-function ChangeColor(element, color, default_color, value) {
-    if(value && element != null){
-        element.style.backgroundColor = color;
-    } else {
-        element.style.backgroundColor = default_color;
-    }
-}
+const changeColor = (element, color, default_color, value) => {
+  if (value && element != null) {
+    element.style.backgroundColor = color;
+  } else {
+    element.style.backgroundColor = default_color;
+  }
+};
 
 //Applies Antigram features depending on the options selected and the current path
 function ApplyAntigram() {
-    let path = window.location.pathname;
-    UpdateOptions(true);
+  let path = window.location.pathname;
+  updateOptions(true);
 
-    //Where Antigram is not needed
-    if( path.slice(0,7) == "/about/" || path.slice(0,11) == "/developer/"){
-        console.log("This path does not need Antigram.");
+  //Where Antigram is not needed
+  if (path.slice(0, 7) == "/about/" || path.slice(0, 11) == "/developer/") {
+    console.log("This path does not need Antigram.");
+  } //Direct Messages Section
+  else if (path.slice(0, 8) == "/direct/") {
+    try {
+      mainSectionElement = document.body.querySelector("section > div > div:last-child > div");
+      navbarElement = document.body.querySelector("section > div > div:first-child"); //div.Hz2lF
 
-    }//Direct Messages Section
-    else if(path.slice(0,8) == "/direct/"){ 
-        try {
-            NavLinks = document.body.querySelector('section > div> div:first-child > div > div:last-child > div')//div._47KiJ 
-            Main = document.body.querySelector('section > div > div:last-child > div');
-            Nav = document.body.querySelector('section > div > div:first-child'); //div.Hz2lF
-        } catch (error) {console.log("Antigram Selector Error 2 -" + error);}
-        BlockElement(NavLinks.children[2], option_values[0]);
-        ChangeColor(Nav, option_values[4], "#fafafa", option_values[3]); 
-        ChangeColor(Main, option_values[4], "#fff", option_values[3]);   
+      blockElement(selectNavLinkExplore(), option_values[0]);
+      changeColor(navbarElement, option_values[4], "#fafafa", option_values[3]);
+      changeColor(mainSectionElement, option_values[4], "#fff", option_values[3]);
+    } catch (error) {
+      console.log("Antigram Selector Error 2 -" + error);
+    }
 
     //Stories Section
-    } else if(path.slice(0,8) == "/stories"){
-        try {
-            Main = document.body.querySelector('section > div > div');
-        } catch (error) {console.log("Antigram Selector Error 3 -" + error);}
-        ChangeColor(Main, option_values[4], "#262626", option_values[3]);
-    
+  } else if (path.slice(0, 8) == "/stories") {
+    try {
+      mainSectionElement = document.body.querySelector("section > div > div");
+      changeColor(mainSectionElement, option_values[4], "#262626", option_values[3]);
+    } catch (error) {
+      console.log("Antigram Selector Error 3 -" + error);
+    }
+
     //Explore and Directory Sections
-    } else if(path.slice(0,9) == "/explore/" || path.slice(0,11) == "/directory/"){
-        try {
-            Main = document.body.querySelector('[role=main]');
-            Nav = document.body.querySelector('nav > div:last-child > div'); //div.Hz2lF
-        } catch(error) {console.log("Antigram Selector Error 4 -" + error);}
-        BlockElement(Main, option_values[0]);
-        ChangeColor(Nav, option_values[4], "#fafafa", option_values[3]);
+  } else if (path.slice(0, 9) == "/explore/" || path.slice(0, 11) == "/directory/") {
+    try {
+      mainSectionElement = document.body.querySelector("[role=main]");
+      navbarElement = document.body.querySelector("nav > div:last-child > div"); //div.Hz2lF
+      blockElement(mainSectionElement, option_values[0]);
+      changeColor(navbarElement, option_values[4], "#fafafa", option_values[3]);
+    } catch (error) {
+      console.log("Antigram Selector Error 4 -" + error);
+    }
 
     //Home Section
-    } else if(path == "/"){
-        try {
-            NavLinks = document.body.querySelector('nav > div:last-child > div > div > div:last-child > div')//div._47KiJ 
-            Suggestions = document.body.querySelector('main > section > div:nth-child(3) > div:nth-child(2)') //div._8UZ6e
-            Stories = document.body.querySelector('main > section > div:first-child > div:first-child') //div.VideM
-            Main = document.body.querySelector('[role=main]');
-            Nav = document.body.querySelector('nav > div:last-child > div'); //div.Hz2lF
-            Feed = document.body.querySelector('main > section > div:first-child > div:nth-child(2)')
-            FeedLoader = document.body.querySelector('main > section > div:first-child > div:nth-child(3)')
-        } catch(error) {console.log("Antigram Selector Error 5 -" + error);}
-        BlockElement(NavLinks.children[3], option_values[0]);
-        BlockElement(Suggestions, option_values[1]);
-        BlockElement(Stories, option_values[2]);
-        BlockElement(Feed, option_values[5]);
-        BlockElement(FeedLoader, option_values[5]);
-        ChangeColor(Main, option_values[4], "#fff", option_values[3]);
-        ChangeColor(Nav, option_values[4], "#fafafa", option_values[3]);
-        ChangeColor(Stories, option_values[4], "#fff", option_values[3]);
+  } else if (path == "/") {
+    try {
+      mainSectionElement = document.body.querySelector("[role=main]");
+      navbarElement = document.body.querySelector("nav > div:last-child > div"); //div.Hz2lF
+      feedBackgroundElement = document.querySelector("main section div");
+      blockElement(selectNavLinkExplore(), option_values[0]);
+      blockElement(selectSuggestionsBox(), option_values[1]);
+      blockElement(selectSuggestionsLink(), option_values[1]);
+      blockElement(selectStoriesBox(), option_values[2]);
+      blockElement(selectStoriesArrow(), option_values[2]);
+      blockElement(selectFeedBox(), option_values[5]);
+      blockElement(selectFeedLoader(), option_values[5]);
+      changeColor(mainSectionElement, option_values[4], "#fff", option_values[3]);
+      changeColor(navbarElement, option_values[4], "#fafafa", option_values[3]);
+      changeColor(feedBackgroundElement, option_values[4], "#fff", option_values[3]);
+      changeColor(selectStoriesBox(), option_values[4], "#fff", option_values[3]);
+    } catch (error) {
+      console.log("Antigram Selector Error 5 -" + error);
+    }
 
     //General Case
-    } else {
-        try {
-            NavLinks = document.body.querySelector('nav > div:last-child > div > div > div:last-child > div')//div._47KiJ 
-            Main = document.body.querySelector('[role=main]');
-            Nav = document.body.querySelector('nav > div:last-child > div'); //div.Hz2lF
-        } catch(error) {console.log("Antigram Selector Error 1 -" + error);}
-        BlockElement(NavLinks.children[3], option_values[0]);
-        ChangeColor(Main, option_values[4], "#fff", option_values[3]);
-        ChangeColor(Nav, option_values[4], "#fafafa", option_values[3]);
+  } else {
+    try {
+      mainSectionElement = document.body.querySelector("[role=main]");
+      navbarElement = document.body.querySelector("nav > div:last-child > div"); //div.Hz2lF
+      blockElement(selectNavLinkExplore(), option_values[0]);
+      changeColor(mainSectionElement, option_values[4], "#fff", option_values[3]);
+      changeColor(navbarElement, option_values[4], "#fafafa", option_values[3]);
+    } catch (error) {
+      console.log("Antigram Selector Error 1 -" + error);
     }
+  }
 }
 
-UpdateOptions();
+updateOptions();
 
 //Applies the changes whenever the options are modified
 chrome.storage.onChanged.addListener(ApplyAntigram);
--- options.js
+++ options.js
@@ -1,30 +1,36 @@
-
 //We get the elements that will trigger functions
-let explore = document.getElementById('explore');
-let suggestions = document.getElementById('suggestions');
-let stories = document.getElementById('stories');
-let bg_color = document.getElementById('bg_color');
-let color = document.getElementById('color');
-let feed = document.getElementById('feed');
-let saved_text = document.getElementById('saved_text');
+let explore = document.getElementById("explore");
+let suggestions = document.getElementById("suggestions");
+let stories = document.getElementById("stories");
+let bg_color = document.getElementById("bg_color");
+let color = document.getElementById("color");
+let feed = document.getElementById("feed");
+let saved_text = document.getElementById("saved_text");
 
-let about = document.getElementById('about_link');
-let settings = document.getElementById('settings_link');
+let about = document.getElementById("about_link");
+let settings = document.getElementById("settings_link");
 let about_showing = false;
 
 //Updates option values to the chrome storage
 function updateOptions() {
-  let option_values = [explore.checked,suggestions.checked,stories.checked,color.checked, bg_color.value, feed.checked];
-  chrome.storage.sync.set({options: option_values}, function() {
-    console.log('Value is set to ' + option_values);
+  let option_values = [
+    explore.checked,
+    suggestions.checked,
+    stories.checked,
+    color.checked,
+    bg_color.value,
+    feed.checked,
+  ];
+  chrome.storage.sync.set({ options: option_values }, function () {
+    console.log("Value is set to " + option_values);
   });
   saved_text.innerHTML = "(Changes were saved \u270c)";
 }
 
 //Gets option values saved in chrome storage
 function getOptions() {
-  chrome.storage.sync.get(['options'], function(result) {
-    console.log('Saved values were ' + result.options);
+  chrome.storage.sync.get(["options"], function (result) {
+    console.log("Saved values were " + result.options);
     explore.checked = result.options[0];
     suggestions.checked = result.options[1];
     stories.checked = result.options[2];
@@ -36,17 +42,15 @@ function getOptions() {
 
 //Displays either the "Options" page or the "About" one.
 function toggleAbout() {
-  if(!about_showing){
-    document.getElementById('settings_page').style.display="none";
-    document.getElementById('about_page').style.display="block";
+  if (!about_showing) {
+    document.getElementById("settings_page").style.display = "none";
+    document.getElementById("about_page").style.display = "block";
     about_showing = true;
-  }
-  else {
-    document.getElementById('settings_page').style.display="block";
-    document.getElementById('about_page').style.display="none";
+  } else {
+    document.getElementById("settings_page").style.display = "block";
+    document.getElementById("about_page").style.display = "none";
     about_showing = false;
   }
-  
 }
 
 //Adds event listeners to all buttons
@@ -57,11 +61,8 @@ color.addEventListener("change", updateOptions);
 bg_color.addEventListener("change", updateOptions);
 feed.addEventListener("change", updateOptions);
 
-
 settings.addEventListener("click", toggleAbout);
 about.addEventListener("click", toggleAbout);
 
 //Applies saved options so that the inputs show the actual values
 getOptions();
-
-
