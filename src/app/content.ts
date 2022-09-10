d08fb2f18ef5ae1b5ee30bc314365ca9d44edd25
marcsaga <53085891+marcsaga@users.noreply.github.com>
2022-09-10 18:11:43 +0200

    Set typescript configuration + folder restructure (#31)
    Co-authored-by: aymyo <aymyo@users.noreply.github.com>
    
    Co-authored-by: aymyo <aymyo@users.noreply.github.com>

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,29 +1,16 @@
-const DEBUG_ON = true;
+import { hideElement, changeColor } from "./helpers/modifiers";
+import * as select from "./helpers/selectors";
+
+const DEBUG_ON = false;
 
 //An array that will store the option values:
 //[explore-toggle,suggestions-toggle,stories,color-toggle,hexadecimal color code]
-let option_values;
+let option_values: (string | boolean)[];
 
 // DOM elements that vary between pages
-let mainSectionElement;
-let navbarElement;
-let feedBackgroundElement;
-
-const selectNavLinkDirect = () => document.querySelector('a[href*="/direct/inbox/"]');
-const selectNavLinkActivity = () => document.querySelector('a[href*="/accounts/activity"]');
-const selectNavLinkExplore = () => document.querySelector('section > nav a[href*="/explore/"]');
-
-const selectSuggestionsBox = () =>
-  document.querySelector('a[href*="/explore/people/"]').closest("div").nextSibling;
-const selectSuggestionsLink = () =>
-  document.querySelector('a[href*="/explore/people/"]').closest("div");
-
-const selectStoriesBox = () => document.querySelectorAll('div[role="presentation"]')[0];
-const selectStoriesArrow = () =>
-  document.querySelectorAll('div[role="presentation"]')[0].nextSibling;
-
-const selectFeedBox = () => document.querySelector("article").closest("div");
-const selectFeedLoader = () => document.querySelector('[data-visualcompletion="loading-state"]');
+let mainSectionElement: HTMLElement | null;
+let navbarElement: HTMLElement | null;
+let feedBackgroundElement: HTMLElement | null;
 
 //This function gets the most recent option values
 const updateOptions = () => {
@@ -34,7 +21,7 @@ const updateOptions = () => {
     }
     //Default initial values
     if (!option_values) {
-      let option_values = [true, true, false, false, "#fafafa", false];
+      const option_values = [true, true, false, false, "#fafafa", false];
       chrome.storage.sync.set({ options: option_values }, function () {
         console.info("Antigram's default values set to:" + option_values);
       });
@@ -42,28 +29,10 @@ const updateOptions = () => {
   });
 };
 
-//This function blocks or shows the selected element depending on the option_value
-const blockElement = (element, value) => {
-  if (value && element != null) {
-    element.style.display = "none";
-  } else if (element != null) {
-    element.style.display = "block";
-  }
-};
-
-//Changes Background Color if the setting is activated / restores the original if not
-const changeColor = (element, color, default_color, value) => {
-  if (value && element != null) {
-    element.style.backgroundColor = color;
-  } else {
-    element.style.backgroundColor = default_color;
-  }
-};
-
 //Applies Antigram features depending on the options selected and the current path
 function ApplyAntigram() {
-  let path = window.location.pathname;
-  updateOptions(true);
+  const path = window.location.pathname;
+  updateOptions();
 
   //Where Antigram is not needed
   if (path.slice(0, 7) == "/about/" || path.slice(0, 11) == "/developer/") {
@@ -73,8 +42,10 @@ function ApplyAntigram() {
     try {
       mainSectionElement = document.body.querySelector("section > div > div:last-child > div");
       navbarElement = document.body.querySelector("section > div > div:first-child"); //div.Hz2lF
-      const exploreLinkInDirectPage = document.querySelector('section a[href*="/explore/"]');
-      blockElement(exploreLinkInDirectPage, option_values[0]);
+      const exploreLinkInDirectPage: HTMLElement | null = document.querySelector(
+        'section a[href*="/explore/"]'
+      );
+      hideElement(exploreLinkInDirectPage, option_values[0]);
       changeColor(navbarElement, option_values[4], "#fafafa", option_values[3]);
       changeColor(mainSectionElement, option_values[4], "#fff", option_values[3]);
     } catch (error) {
@@ -95,7 +66,7 @@ function ApplyAntigram() {
     try {
       mainSectionElement = document.body.querySelector("[role=main]");
       navbarElement = document.body.querySelector("nav > div:last-child > div"); //div.Hz2lF
-      blockElement(mainSectionElement, option_values[0]);
+      hideElement(mainSectionElement, option_values[0]);
       changeColor(navbarElement, option_values[4], "#fafafa", option_values[3]);
     } catch (error) {
       console.error("Antigram Selector Error 4 -" + error);
@@ -107,17 +78,16 @@ function ApplyAntigram() {
       mainSectionElement = document.body.querySelector("[role=main]");
       navbarElement = document.body.querySelector("nav > div:last-child > div"); //div.Hz2lF
       feedBackgroundElement = document.querySelector("main section div");
-      blockElement(selectNavLinkExplore(), option_values[0]);
-      blockElement(selectSuggestionsBox(), option_values[1]);
-      blockElement(selectSuggestionsLink(), option_values[1]);
-      blockElement(selectStoriesBox(), option_values[2]);
-      blockElement(selectStoriesArrow(), option_values[2]);
-      blockElement(selectFeedBox(), option_values[5]);
-      blockElement(selectFeedLoader(), option_values[5]);
+      hideElement(select.navLinkExplore(), option_values[0]);
+      hideElement(select.suggestionsBox(), option_values[1]);
+      hideElement(select.suggestionsLink(), option_values[1]);
+      hideElement(select.storiesBox(), option_values[2]);
+      hideElement(select.storiesArrow(), option_values[2]);
+      hideElement(select.feedBox(), option_values[5]);
+      hideElement(select.feedLoader(), option_values[5]);
       changeColor(mainSectionElement, option_values[4], "#fff", option_values[3]);
       changeColor(navbarElement, option_values[4], "#fafafa", option_values[3]);
       changeColor(feedBackgroundElement, option_values[4], "#fff", option_values[3]);
-      changeColor(selectStoriesBox(), option_values[4], "#fff", option_values[3]);
     } catch (error) {
       console.error("Antigram Selector Error 5 -" + error);
     }
@@ -127,7 +97,7 @@ function ApplyAntigram() {
     try {
       mainSectionElement = document.body.querySelector("[role=main]");
       navbarElement = document.body.querySelector("nav > div:last-child > div"); //div.Hz2lF
-      blockElement(selectNavLinkExplore(), option_values[0]);
+      hideElement(select.navLinkExplore(), option_values[0]);
       changeColor(mainSectionElement, option_values[4], "#fff", option_values[3]);
       changeColor(navbarElement, option_values[4], "#fafafa", option_values[3]);
     } catch (error) {
